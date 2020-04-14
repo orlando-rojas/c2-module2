51fc55f5d870ce5e663d6bfcd389779ed4f643ff
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-04-14 12:16:54 -0500

    fixed output

--- week-1/day-2/hailstone.rb
+++ week-1/day-2/hailstone.rb
@@ -1,15 +1,15 @@
 def hailstone(x)
     steps = 0
     until x==1
+        puts "step #{steps} => #{x}"
         if(x%2 == 0)
-            x /= 2
-            steps += 1
+            x /= 2          
         else
             x = (x*3)+1
-            steps += 1
         end
+        steps += 1
     end
-    steps
+    puts "final step #{steps} => #{x}"
 end
 
-puts hailstone(3)
+hailstone(3)
