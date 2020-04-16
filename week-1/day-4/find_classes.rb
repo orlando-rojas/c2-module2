7c10259bc7b0e4ef0e110c9002c13741a107a081
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-04-16 11:37:21 -0500

    Find the classes!

---
+++ week-1/day-4/find_classes.rb
@@ -0,0 +1,22 @@
+class Laptop
+    def initialize(color, brand, os)
+        @color = color
+        @brand = brand
+        @os = os
+    end
+end
+
+class Cup
+    def initialize(size,color)
+        @size = size
+        @color = color
+    end
+end
+
+class Desk
+    def initialize(material, color, number_of_drawers )
+        @material = material
+        @color = color
+        @number_of_drawers = number_of_drawers
+    end
+end
