4a619fa4fd6196516e0825bce47ee96004f12094
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-04-20 11:45:18 -0500

    Put N

--- week-2/day-1/put_n.rb
+++ week-2/day-1/put_n.rb
@@ -1,14 +1,9 @@
 def put_n(str,n)
     raise ArgumentError.new(
         "Second argument is not a positive Integer"
-    ) if (!n.is_a? Integer) || (n < 0)
+    ) if (!n.is_a? Integer) || (n <= 0)
 
-    i = 0
-    while i<n
-        puts str
-        i += 1
-    end
-
+   n.times {puts str}
 end
 
-put_n("This will be printed 5 times!", 5)
+put_n("This will be printed 5 times!", 0)
