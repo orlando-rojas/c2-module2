4816cae666e5b4e06278782f549136374307c05c
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-04-20 11:48:15 -0500

    Echo

--- week-2/day-1/echo.rb
+++ week-2/day-1/echo.rb
@@ -1,24 +1,18 @@
-begin
-    def put_n(str,n)
-        raise ArgumentError.new(
-            "Second argument is not a positive Integer"
-        ) if (!n.is_a? Integer) || (n < 0)
+def put_n(str,n)
+    raise ArgumentError.new(
+        "Second argument is not a positive Integer"
+    ) if (!n.is_a? Integer) || (n <= 0)
     
-        i = 0
-        while i<n
-            puts str
-            i += 1
-        end
-    end    
+    n.times {puts str}
+end
 
+begin
     print "What do you want to echo? "
     str = gets.chomp
     print "How many times do you want to repeat it? "
     n = Integer(gets.chomp)
 
     put_n(str,n)
-
-
 rescue ArgumentError
     p "I don't understand how many times do you want to echo the string!"
     retry
