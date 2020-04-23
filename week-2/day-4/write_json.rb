31710678eb5cfbc406d390ab13fcc32562848efb
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-04-23 16:50:56 -0500

    Write JSON notes

---
+++ week-2/day-4/write_json.rb
@@ -0,0 +1,8 @@
+require 'json'
+notes = [
+  {title: "Investing retro" ,text: "I wish I had invested in bitcoin when it was worth 500$"},
+  {title: "Investing retro (2)" ,text: "I wish I had never stopped my bitcoin farm."},
+  {title: "Investing retro (3)" ,text: "I wish I hadn't bought bitcoin at 13k, to then sell at 4k"},
+  {title: "Investing retro (4)" ,text: "I wish I had sold my 8 ETH when it was at 700$ instead of 100$"},
+]
+File.open("notes.json","w") {|x |x.write(notes.to_json)}
