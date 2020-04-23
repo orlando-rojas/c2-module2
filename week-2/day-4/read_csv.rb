7d3112a0a49f8150bc5118ead01f8483d0d3ee18
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-04-23 15:26:18 -0500

    Read a CSV

---
+++ week-2/day-4/read_csv.rb
@@ -0,0 +1,11 @@
+require 'csv'
+grades_arr = []
+grades = CSV.read("grades.csv", :quote_char => "|")
+
+h = Hash.new(0)
+grades.each do |row|
+    h[row[1]] = row[7].to_i
+end
+a = h.sort_by { |k, v| -v }.take(3)
+i = 0
+a.each {|x,y| puts "#{i+=1} - #{x} with a final grade of #{y}"}
