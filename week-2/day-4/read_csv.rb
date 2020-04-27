4dec098a54d89f7455c1246b232ef1d3e25a1b6d
orlando-rojas <rojasmoncada.orlando@gmail.com>
2020-04-27 10:26:01 -0500

    changes

--- week-2/day-4/read_csv.rb
+++ week-2/day-4/read_csv.rb
@@ -5,6 +5,6 @@ h = Hash.new(0)
 grades.each do |row|
     h[row[1]] = row[7].to_i
 end
-a = h.sort_by { |k, v| -v }.take(3)
+p a = h.sort_by { |k, v| -v }
 i = 0
 a.each {|x,y| puts "#{i+=1} - #{x} with a final grade of #{y}"}
